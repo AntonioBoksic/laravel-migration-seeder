bd00d09004acc007526df9add560160ce5d9d65d
Antonio <antonioboksic1603@gmail.com>
2023-07-13 23:33:51 +0200

    fatto anche seeder, mi sembra che c'è qualche errore perchè il database rimane vuoto anche se non mi dà errori nel terminale. Proviamo a debuggare.

---
+++ laravel-migration-seeder-project/database/seeders/JourneyTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+                            //questo lo aggiungiamo noi
+use App\Models\House;
+
+class JourneyTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        House :: factory -> count(20) -> create()
+    }
+}
